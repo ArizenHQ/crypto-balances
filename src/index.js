8023a8789e4d63db8d78aead1b736a604aff1bfb
Alexandre Ouicher <aouicher@users.noreply.github.com>
2023-08-28 13:09:46 +0200

    list CORS

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const balance = require("./crypto-balance");
+var allowlist = ['http://localhost', 'https://staging.custody.coinhouse.com', 'https://custody.coinhouse.com'];
 
-app.get('/:coin/:addr', function (req, res) {
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
+
+app.get('/:coin/:addr', cors(corsOptionsDelegate), function (req, res) {
     balance(req.params.addr, req.params.coin)
     .then(items => res.send(items))
     .catch(error => res.send({ "error": error.message }));
 });
 
-app.get('/:addr', function (req, res) {
+app.get('/:addr', cors(corsOptionsDelegate), function (req, res) {
     balance(req.params.addr)
     .then(items => res.send(items))
     .catch(error => res.send({ "error": error.message }));
 });
 
-app.use('*', function (req, res) {
+app.use('*', cors(corsOptionsDelegate), function (req, res) {
     res.send({ "error": "invalid route" });
 });
 
